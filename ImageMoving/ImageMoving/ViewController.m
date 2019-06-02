ce411ad1e4ea10dd2700b39fc7ee66e751eba101
Hanna Rybakova <hanna.rybakowa@gmail.com>
2019-06-03 00:21:10 +0300

    moving custom view

--- ImageMoving/ImageMoving/ViewController.m
+++ ImageMoving/ImageMoving/ViewController.m
@@ -10,7 +10,7 @@
 #import "SecondViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, weak) UIView *movingView;
 @end
 
 @implementation ViewController
@@ -19,7 +19,7 @@
     [self.navigationController popViewControllerAnimated:YES];
     [self.view addSubview:data];
     [self setCustomViewConstraints:data];
-    [self setTitle:data.title];
+    //[self setTitle:data.title];
 }
 
 - (void)setCustomViewConstraints:(CustomView*)data {
@@ -33,8 +33,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    [self setTitle:@""];
+    [self setTitle:@"ImageMoving"];
+    self.view.multipleTouchEnabled = NO;
     
     UIBarButtonItem *showAddButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(pressAdd:)];
     
@@ -49,4 +49,37 @@
     vc.delegate = self;
     [self.navigationController pushViewController:vc animated:YES];
 }
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView:self.view];
+    
+    //determine view selected
+    UIView *view = [self.view hitTest:point withEvent:event];
+    if (![view isEqual:self.view]) {
+        NSString *string = [(CustomView *)view title];
+        [self setTitle:string];
+        self.movingView = view;
+    } else {
+        [self setTitle:@"ImageMoving"];
+        self.movingView = nil;
+    }
+    
+}
+    -(void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+        if (self.movingView) {
+            UITouch *touch = [touches anyObject];
+            CGPoint point = [touch locationInView:self.view];
+            self.movingView.center = point;
+        }
+    }
+
+-(void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    self.movingView = nil;
+}
+
+-(void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    self.movingView = nil;
+}
+
 @end
